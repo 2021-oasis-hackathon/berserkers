3608f5115f412c89c6c0050a059483bccfe42683
Moon Bong-oh <moonbonoz@gmail.com>
2021-08-17 14:16:03 +0000

    add: 정말 간단한 Footer 추가 - #6

---
+++ app/src/components/Footers/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+  <footer class="relative bg-blueGray-200 pt-8 pb-6">
+    <div
+      class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
+      style="transform: translateZ(0);"
+    >
+      <svg
+        class="absolute bottom-0 overflow-hidden"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="none"
+        version="1.1"
+        viewBox="0 0 2560 100"
+        x="0"
+        y="0"
+      >
+        <polygon
+          class="text-blueGray-200 fill-current"
+          points="2560 0 2560 100 0 100"
+        ></polygon>
+      </svg>
+    </div>
+    <div class="container mx-auto px-4">
+      <hr class="my-6 border-blueGray-300" />
+      <div
+        class="flex flex-wrap items-center md:justify-between justify-center"
+      >
+        <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+          <div class="text-sm text-blueGray-500 font-semibold py-1">
+            Copyright © {{ date }} 호나므로 by
+            <a
+              href="https://github.com/2021-oasis-hackathon/berserkers"
+              class="text-blueGray-500 hover:text-blueGray-800"
+            >
+              광전사들
+            </a>
+            .
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    };
+  },
+};
+</script>
