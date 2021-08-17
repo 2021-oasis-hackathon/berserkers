3004b86b255cb382953ed9b7a88fd16cdd6fc584
Moon Bong-oh <moonbonoz@gmail.com>
2021-08-17 14:03:56 +0000

    update: tailwind css에 맞는 App Initailizing과 tailwind css minimal version 가져옴 - #1

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,5 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
-
-<script>
-import HelloWorld from '@components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-}
-</script>
