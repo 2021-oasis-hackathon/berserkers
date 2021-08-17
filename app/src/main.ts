7030333b805c4f6251b6719390b098bc622a53d2
Moon Bong-oh <moonbonoz@gmail.com>
2021-08-17 14:01:24 +0000

    update: vuetify에서 tailwind css로 변경 - #1

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,34 @@
-import { createApp } from 'vue'
-import vuetify from './plugins/vuetify'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createWebHistory, createRouter } from 'vue-router';
 
-const app = createApp(App)
-app.use(vuetify)
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "@assets/styles/index.css"
+import "@assets/styles/tailwind.css";
 
-app.mount('#app')
+import App from '@/App.vue';
+
+import Index from '@/pages/Index.vue';
+import PersonalProgram from '@/pages/PersonalProgram.vue';
+import LocalCommunityProgram from '@/pages/LocalCommunityProgram.vue';
+import ProgramManagement from '@/pages/ProgramManagement.vue';
+
+const routes = [{
+    path: "/",
+    component: Index 
+}, {
+    path: "/personal-programs",
+    component: PersonalProgram 
+}, {
+    path: "/local-community-programs",
+    component: LocalCommunityProgram 
+}, {
+    path: "/program-management",
+    component: ProgramManagement 
+}];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+createApp(App).use(router).mount('#app');
--- app/vite.config.js
+++ app/vite.config.js
@@ -3,21 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 import path from 'path'
 
-// const srcPath = path.resolve(__dirname, 'src', 'styles', 'variables.scss')
+// const srcPath = path.resolve(__dirname, 'src/assets/styles', 'variables.scss')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   define: { 'process.env': {} },
-  css: {
-    preprocessorOptions: {
-      // sass: { additionalData: `@import ${srcPath}\n` },
-      // scss: { additionalData: `@import ${srcPath};\n` },
-    },
-  },
   resolve: {
       alias: {
+        '@': path.join(__dirname, 'src'),
         '@components': path.join(__dirname, 'src/components'),
+        '@views': path.join(__dirname, 'src/views'),
+        '@layouts': path.join(__dirname, 'src/layouts'),
+        '@pages': path.join(__dirname, 'src/pages'),
         '@assets': path.join(__dirname, 'src/assets')
       }
   }
