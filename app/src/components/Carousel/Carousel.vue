e0023aaafdb5490acb3a275ebd0b24480380f04f
Moon Bong-oh <moonbonoz@gmail.com>
2021-08-18 11:34:30 +0000

    update: 각 프로그램 별로 Carousel을 달아주고 자잘한거 수정 - #6 - TODO: 로컬 커뮤니티 프로그램들을 가져오는 DB 코드 만들어야 함

---
+++ app/src/components/Carousel/Carousel.vue
@@ -0,0 +1,41 @@
+<template>
+  <carousel>
+    <slide v-for="item in items" :key="item.id">
+      <slot :item="item"></slot>
+    </slide>
+    <template #addons>
+      <navigation>
+      </navigation>
+    </template>
+  </carousel>
+</template>
+
+<style>
+.carousel__prev,
+.carousel__next {
+	border: 1px solid lightgray;
+	background-color: white;
+    color: rgb(34, 34, 34)
+}
+</style>
+
+<script lang="ts">
+import "vue3-carousel/dist/carousel.css";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+
+export default {
+  props: {
+    items: {
+      type: Array as () => Array<any>,
+      required: true,
+      default: () => [],
+    },
+  },
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+};
+</script>
--- app/src/pages/Index.vue
+++ app/src/pages/Index.vue
@@ -16,7 +16,10 @@
       >
         <div
           class="absolute top-0 w-full h-full bg-center bg-no-repeat bg-cover"
-          style="background-image: url('/src/assets/images/banner005.jpg'); background-color: #feead2;"
+          style="
+            background-image: url('/src/assets/images/banner005.jpg');
+            background-color: #feead2;
+          "
         >
           <span
             id="blackOverlay"
@@ -62,33 +65,110 @@
           >
             <polygon
               class="fill-current"
-              style="color: #f5f7f6;"
+              style="color: #ffffff"
               points="2560 0 2560 100 0 100"
             ></polygon>
           </svg>
         </div>
       </div>
 
+      <section class="pt-20 pb-48">
+        <div class="container mx-auto px-4">
+          <div class="flex flex-wrap justify-flex-start mb-8">
+            <div class="w-full lg:w-6/12 px-4">
+              <h2 class="text-3xl font-semibold text-nowrap">
+                사람들과 모여 한달살이 기획
+              </h2>
+            </div>
+          </div>
+          <div class="flex flex-wrap">
+            <carousel
+              class="w-full px-6 py-2"
+              :itemsToShow="4"
+              :snapAlign="'end'"
+              :items="personalPrograms"
+            >
+              <template v-slot:default="slotProps">
+                <div class="mt-4 mb-4 px-2">
+                  <personal-program-card :item="slotProps.item" />
+                </div>
+              </template>
+            </carousel>
+          </div>
+        </div>
+        <div class="container mx-auto px-4 pt-20">
+          <div class="flex flex-wrap justify-flex-start mb-8">
+            <div class="w-full lg:w-6/12 px-4">
+              <h2 class="text-3xl font-semibold">지역 공동체 프로그램</h2>
+            </div>
+          </div>
+          <div class="flex flex-wrap">
+            <carousel
+              class="w-full px-6 py-2"
+              :itemsToShow="4"
+              :snapAlign="'end'"
+              :items="personalPrograms"
+            >
+              <template v-slot:default="slotProps">
+                <div class="mt-4 mb-4 px-2">
+                  <local-community-program-card :item="slotProps.item" />
+                </div>
+              </template>
+            </carousel>
+          </div>
+        </div>
+      </section>
+
       <section
         class="header relative pt-16 items-center flex h-screen max-h-860-px"
-        style="background-color: #f5f7f6;"
+        style="background-color: #f5f7f6"
       >
+        <div
+          class="
+            bottom-auto
+            top-0
+            left-0
+            right-0
+            w-full
+            absolute
+            pointer-events-none
+            overflow-hidden
+            -mt-20
+            h-20
+          "
+          style="transform: translateZ(0)"
+        >
+          <svg
+            class="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon
+              class="fill-current"
+              style="color: #f5f7f6"
+              points="2560 0 2560 100 0 100"
+            ></polygon>
+          </svg>
+        </div>
         <div class="container mx-auto items-center flex flex-wrap">
           <div class="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
             <div class="pt-32 sm:pt-0">
               <h2 class="font-semibold text-4xl text-blueGray-600">
-                마음 맞는 사람들과 함께<br/>
+                마음 맞는 사람들과 함께<br />
                 한달살이에 도전해보세요!
               </h2>
               <p class="mt-4 text-lg leading-relaxed text-blueGray-500">
-                지역 공동체에서 제공해주는 프로그램 뿐만 아니라<br/>
-                다양한 사람들과 모여 기획하고 마을과 연계하여 <br/>
+                지역 공동체에서 제공해주는 프로그램 뿐만 아니라<br />
+                다양한 사람들과 모여 기획하고 마을과 연계하여 <br />
                 한달살이를 진행할 수 있습니다.
               </p>
               <div class="mt-12">
                 <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/vue/overview/notus?ref=vn-index"
-                  target="_blank"
+                  href="/program-management"
                   class="
                     get-started
                     text-white
@@ -114,7 +194,7 @@
                   모임 생성
                 </a>
                 <a
-                  href="https://github.com/creativetimofficial/vue-notus?ref=vn-index"
+                  href="/personal-programs"
                   class="
                     github-star
                     ml-1
@@ -137,7 +217,6 @@
                     transition-all
                     duration-150
                   "
-                  target="_blank"
                 >
                   모임 알아보기
                 </a>
@@ -168,30 +247,35 @@
   </div>
 </template>
 <script>
-import { reactive, onMounted } from 'vue';
-import { getAllPersonalProgram } from "@/firebase"
+import { reactive, ref, onMounted } from "vue";
+import { getAllPersonalProgram } from "@/firebase";
 
 import Navbar from "@/components/Navbars/NavBar.vue";
 import FooterComponent from "@components/Footers/Footer.vue";
+import Carousel from "@components/Carousel/Carousel.vue";
+import PersonalProgramCard from "@components/Cards/PersonalProgramCard.vue";
+import LocalCommunityProgramCard from "@components/Cards/LocalCommunityProgramCard.vue";
 
 import bannerImg from "@assets/images/banner004.jpg";
 import componentBtn from "@assets/img/component-btn.png";
 
 export default {
   setup() {
-    const personalPrograms = reactive([]);
+    const personalPrograms = ref([]);
 
     onMounted(async () => {
       const programs = await getAllPersonalProgram();
-      console.log(programs);
-      // personalPrograms = programs;
-    })
+      personalPrograms.value = programs;
+    });
 
-    return { personalPrograms, bannerImg, componentBtn }
+    return { personalPrograms, bannerImg, componentBtn };
   },
   components: {
     Navbar,
     FooterComponent,
+    Carousel,
+    PersonalProgramCard,
+    LocalCommunityProgramCard,
   },
 };
 </script>
