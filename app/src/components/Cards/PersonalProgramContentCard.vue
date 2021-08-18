f62e3bd426815ffa91421bd0f542ae3a20076530
Moon Bong-oh <moonbonoz@gmail.com>
2021-08-18 18:03:43 +0000

    complete: 개인 프로그램 신청 뷰 - #6

---
+++ app/src/components/Cards/PersonalProgramContentCard.vue
@@ -0,0 +1,101 @@
+<template>
+  <div
+    class="
+      relative
+      flex flex-col
+      min-w-0
+      break-words
+      w-full
+      mb-6
+      shadow-xl
+      rounded-lg
+      bg-blueGray-100
+      border-0
+    "
+  >
+    <div class="rounded-t bg-white mb-0 px-6 py-6">
+      <div class="text-center flex justify-between">
+        <h6 class="text-blueGray-700 text-xl font-bold">모임 조건</h6>
+      </div>
+    </div>
+    <div class="flex-auto px-8 py-10 pt-6">
+      <div class="mt-4 border-blueGray-200">
+        <div class="flex flex-wrap justify-center">
+          <div class="w-full">
+            <p class="mb-4 text-md leading-relaxed text-blueGray-700">
+              <strong>
+                벽화를 못 그리셔도 좋습니다. 코로나 시국으로 인해 모여서
+                회의하기 힘들지만 회의에 잘 참여하고 벽화 그릴 지역이 결정되면
+                언제든지 가실 수 있는 분을 구합니다.
+              </strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div
+    class="
+      relative
+      flex flex-col
+      min-w-0
+      break-words
+      w-full
+      mb-6
+      shadow-2xl
+      rounded-lg
+      bg-blueGray-100
+      border-0
+    "
+  >
+    <div class="rounded-t bg-white mb-0 px-6 py-6">
+      <div class="text-center flex justify-between">
+        <h6 class="text-blueGray-700 text-xl font-bold">프로그램 소개</h6>
+      </div>
+    </div>
+    <div class="flex-auto px-8 py-10 pt-6">
+      <div class="mt-4 border-blueGray-200">
+        <div class="flex flex-wrap justify-center">
+          <div class="w-full">
+            <p class="mb-4 text-md leading-relaxed text-blueGray-700">
+              낙후된 지역에 벽화를 그려서 마을을 아름답게 재 탄생시키는 벽화
+              봉사를 모집합니다.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="py-2 border-blueGray-200">
+        <div class="flex flex-wrap justify-center">
+          <div class="w-full">
+            <p class="mb-4 text-md leading-relaxed text-blueGray-700">
+              안녕하세요, 지스트에 다니는 김소정이라고 합니다! 최근 벽화마을로
+              놀러갔는데, 침체되었던 마을을 벽화 하나만으로 이렇게 아름다운
+              마을로 되살리고 심지어는 관광상품으로 만들어낼 수 있다는 사실이
+              너무 신기하고 좋은 아이디어라고 생각했어요. 벽화마을의 그
+              아기자기함도 너무 좋아해서 관광지에 갔는데 벽화마을이 있다고 하면
+              꼭 들리는 편인데, 문득 어렸을 때 저도 벽화 봉사에 참여해 본 기억이
+              나서 이렇게 벽화 봉사를 기획해 보게 되었어요! 저와 함께 10월
+              1일부터 10월 30일까지 20일정도 마을에서 머물면서 벽화도 그리고,
+              마을에 계신 분들과 이런저런 얘기도 하면서 지낼 25분을 구합니다!
+              아무래도 그림을 그리는 일이다 보니까, 미대 분들을 주로 받고
+              싶어요. 다만 그 외에도 할 일은 많으니 (페인트 통 나르기, 밑색
+              깔기… 등) 그림을 못 그리는 분들도 많은 관심 부탁해요! 감사합니다.
+              문의는 옆 버튼을 클릭해주세요.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex flex-wrap justify-center">
+        <div class="w-full flex justify-center">
+          <img
+            alt="..."
+            src="https://images.unsplash.com/photo-1563019786-60d26496bc65?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80"
+            class="w-full rounded-md shadow-xl"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
