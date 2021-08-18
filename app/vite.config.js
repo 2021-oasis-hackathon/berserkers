38fd9a93b9a9782cff4f405707122849638456aa
Moon Bong-oh <moonbonoz@gmail.com>
2021-08-18 00:36:24 +0000

    update: node process 객체 및 firebase를 사용하기 위한 설정 진행

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,35 +1,37 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import path from 'path'
+import path from "path";
 
 // const srcPath = path.resolve(__dirname, 'src/assets/styles', 'variables.scss')
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  define: { 'process.env': {} },
-  resolve: {
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [vue()],
+    define: { "process.env": { ...process.env, ...loadEnv(mode, process.cwd()) } },
+    resolve: {
       alias: {
-        '@': path.join(__dirname, 'src'),
-        '@components': path.join(__dirname, 'src/components'),
-        '@views': path.join(__dirname, 'src/views'),
-        '@layouts': path.join(__dirname, 'src/layouts'),
-        '@pages': path.join(__dirname, 'src/pages'),
-        '@assets': path.join(__dirname, 'src/assets')
-      }
-  }
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
-  },
-  */
-})
+        "@": path.join(__dirname, "src"),
+        "@components": path.join(__dirname, "src/components"),
+        "@views": path.join(__dirname, "src/views"),
+        "@layouts": path.join(__dirname, "src/layouts"),
+        "@pages": path.join(__dirname, "src/pages"),
+        "@assets": path.join(__dirname, "src/assets"),
+      },
+    },
+    /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+    resolve: {
+      extensions: [
+        '.js',
+        '.json',
+        '.jsx',
+        '.mjs',
+        '.ts',
+        '.tsx',
+        '.vue',
+      ]
+    },
+    */
+  });
+};
