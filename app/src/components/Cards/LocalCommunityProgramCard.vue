2322eb9db29979ef3c91492fd7e2418b5cbb96d4
Moon Bong-oh <moonbonoz@gmail.com>
2021-08-18 11:31:54 +0000

    add: 프로그램들을 매핑해줄 카드 ui component - #7 - PersonalProgramCard: 개인 프로그램 아이템 뷰 - LocalCommunityProgramCard: 로컬 커뮤니티 프로그램 아이템 뷰(아직 구현 덜 됨)

---
+++ app/src/components/Cards/LocalCommunityProgramCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="p-10">
+    <!--Card 1-->
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img class="w-full" :src="item.imgUrl" alt="Mountain" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xs mb-2">{{item.title}}</div>
+        <p class="text-gray-700 text-xs">
+          {{item.description}}
+        </p>
+      </div>
+      <div class="px-6 pt-4 pb-2">
+        <span
+          class="
+            inline-block
+            bg-gray-200
+            rounded-full
+            px-3
+            py-1
+            text-sm
+            font-semibold
+            text-gray-700
+            mr-2
+            mb-2
+          "
+          >#photography</span
+        >
+        <span
+          class="
+            inline-block
+            bg-gray-200
+            rounded-full
+            px-3
+            py-1
+            text-sm
+            font-semibold
+            text-gray-700
+            mr-2
+            mb-2
+          "
+          >#travel</span
+        >
+        <span
+          class="
+            inline-block
+            bg-gray-200
+            rounded-full
+            px-3
+            py-1
+            text-sm
+            font-semibold
+            text-gray-700
+            mr-2
+            mb-2
+          "
+          >#winter</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  props: {
+    item: {
+      type: Object as any,
+      required: true
+    },
+  }
+};
+</script>
---
+++ app/src/components/Cards/PersonalProgramCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="p-10">
+    <!--Card 1-->
+    <div
+      class="max-w-sm rounded overflow-hidden shadow-lg text-left flex flex-col"
+      style="height: 350px"
+    >
+      <img
+        class="w-full"
+        style="object-fit: cover; height: 150px"
+        :src="item.imgUrl"
+        alt="unknown"
+      />
+      <div class="flex flex-1 flex-col px-3 py-4">
+        <div class="font-bold text-2xs mb-2">{{ item.title }}</div>
+        <p class="text-gray-700 text-xs">
+          {{ item.description }}
+        </p>
+      </div>
+      <div class="px-3 py-2">
+        
+      </div>
+      <div class="px-3 pt-4 pb-2">
+        <span
+          v-for="(item, index) in item.keywords"
+          :key="index"
+          class="
+            inline-block
+            bg-gray-200
+            rounded-full
+            px-3
+            py-1
+            text-sm
+            font-semibold
+            text-gray-700
+            mr-2
+            mb-2
+          "
+        >
+          #{{ item }}
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    item: {
+      type: Object as any,
+      required: true,
+    },
+  },
+};
+</script>
