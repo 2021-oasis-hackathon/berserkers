b125c1d1c19fded8d189b8a2208330f677811286
Moon Bong-oh <moonbonoz@gmail.com>
2021-08-18 14:48:49 +0000

    complete: 프로그램 심플 아이템 다 만듦 - #7

--- app/src/components/Cards/LocalCommunityProgramCard.vue
+++ app/src/components/Cards/LocalCommunityProgramCard.vue
@@ -1,73 +1,117 @@
 <template>
   <div class="p-10">
     <!--Card 1-->
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img class="w-full" :src="item.imgUrl" alt="Mountain" />
-      <div class="px-6 py-4">
-        <div class="font-bold text-xs mb-2">{{item.title}}</div>
-        <p class="text-gray-700 text-xs">
-          {{item.description}}
-        </p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <span
-          class="
-            inline-block
-            bg-gray-200
-            rounded-full
-            px-3
-            py-1
-            text-sm
-            font-semibold
-            text-gray-700
-            mr-2
-            mb-2
-          "
-          >#photography</span
-        >
-        <span
-          class="
-            inline-block
-            bg-gray-200
-            rounded-full
-            px-3
-            py-1
-            text-sm
-            font-semibold
-            text-gray-700
-            mr-2
-            mb-2
-          "
-          >#travel</span
-        >
-        <span
-          class="
-            inline-block
-            bg-gray-200
-            rounded-full
-            px-3
-            py-1
-            text-sm
-            font-semibold
-            text-gray-700
-            mr-2
-            mb-2
-          "
-          >#winter</span
-        >
+    <div
+      class="
+        max-w-sm
+        rounded
+        overflow-hidden
+        shadow-lg
+        text-left
+        flex flex-col flex-1 flex-wrap
+      "
+      style="height: 390px"
+    >
+      <img
+        class="w-full"
+        style="object-fit: cover; height: 150px"
+        :src="data.imgUrl"
+        alt="unknown"
+      />
+      <div class="flex flex-col flex-1 flex-wrap">
+        <div class="flex flex-1 flex-col px-3 my-2">
+          <div id="title" class="font-bold text-emerald-600 text-xs">
+            <i class="fas fa-map-marker-alt"></i>
+            {{ data.location.region }}
+          </div>
+          <div class="font-bold text-2xs mb-1">{{ data.title }}</div>
+          <p id="description" class="text-gray-700 text-xs">
+            {{ data.description }}
+          </p>
+        </div>
+        <div class="flex flex-col px-3 my-2 text-gray-700">
+          <div class="text-xs mb-2">
+            <span class="font-bold">예정기간:</span> {{ data.startDate }} ~
+            {{ data.endDate }}
+          </div>
+          <div class="text-xs mb-2">
+            <span class="font-bold">신청기간:</span> {{ data.deadlineDate }}
+            <span class="text-red-500 font-bold">({{data.applicationPeriod}}일 후 마감)</span>
+          </div>
+          <div class="text-xs">
+            <span class="font-bold">모집인원:</span> {{ data.applicantCount }}명
+            / {{ data.applicantMaxCount }}명
+          </div>
+        </div>
+        <div class="flex flex-row px-3 mt-2 pb-2">
+          <span
+            v-for="(item, index) in data.keywords"
+            :key="index"
+            class="
+              inline-block
+              bg-gray-200
+              rounded-full
+              px-2
+              py-1
+              text-xs
+              font-semibold
+              text-gray-700
+              mr-2
+              mb-2
+            "
+          >
+            #{{ item }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
+<style scoped>
+  #title {
+   overflow: hidden;
+   text-overflow: ellipsis;
+   display: -webkit-box;
+   -webkit-line-clamp: 1; /* number of lines to show */
+   -webkit-box-orient: vertical;
+  }
+
+  #description {
+   overflow: hidden;
+   text-overflow: ellipsis;
+   display: -webkit-box;
+   -webkit-line-clamp: 2; /* number of lines to show */
+   -webkit-box-orient: vertical;
+  }
+</style>
+
 <script lang="ts">
+import { reactive, ref, onMounted, computed } from "vue";
+import moment from "moment";
 
 export default {
   props: {
     item: {
       type: Object as any,
-      required: true
+      required: true,
     },
-  }
+  },
+  setup({ item }) {
+    const data = computed(() => {
+      return {
+        ...item,
+        startDate: moment(item.startDate.toDate()).format("YYYY-MM-DD"),
+        endDate: moment(item.endDate.toDate()).format("YYYY-MM-DD"),
+        deadlineDate: moment(item.deadlineDate.toDate()).format("YYYY-MM-DD"),
+        applicationPeriod: moment(item.deadlineDate.toDate()).diff(
+          moment(),
+          "days"
+        ),
+      };
+    });
+
+    return { data };
+  },
 };
 </script>
--- app/src/components/Cards/PersonalProgramCard.vue
+++ app/src/components/Cards/PersonalProgramCard.vue
@@ -2,55 +2,112 @@
   <div class="p-10">
     <!--Card 1-->
     <div
-      class="max-w-sm rounded overflow-hidden shadow-lg text-left flex flex-col"
-      style="height: 350px"
+      class="
+        max-w-sm
+        rounded
+        overflow-hidden
+        shadow-lg
+        text-left
+        flex flex-col flex-1 flex-wrap
+      "
+      style="height: 375px"
     >
       <img
         class="w-full"
         style="object-fit: cover; height: 150px"
-        :src="item.imgUrl"
+        :src="data.imgUrl"
         alt="unknown"
       />
-      <div class="flex flex-1 flex-col px-3 py-4">
-        <div class="font-bold text-2xs mb-2">{{ item.title }}</div>
-        <p class="text-gray-700 text-xs">
-          {{ item.description }}
-        </p>
-      </div>
-      <div class="px-3 py-2">
-        
-      </div>
-      <div class="px-3 pt-4 pb-2">
-        <span
-          v-for="(item, index) in item.keywords"
-          :key="index"
-          class="
-            inline-block
-            bg-gray-200
-            rounded-full
-            px-3
-            py-1
-            text-sm
-            font-semibold
-            text-gray-700
-            mr-2
-            mb-2
-          "
-        >
-          #{{ item }}
-        </span>
+      <div class="flex flex-col flex-1 flex-wrap">
+        <div class="flex flex-1 flex-col px-3 my-2">
+          <div id="title" class="font-bold text-2xs mb-2">{{ data.title }}</div>
+          <p id="description" class="text-gray-700 text-xs">
+            {{ data.description }}
+          </p>
+        </div>
+        <div class="flex flex-col px-3 my-2 text-gray-700">
+          <div class="text-xs mb-2">
+            <span class="font-bold">예정기간:</span> {{ data.startDate }} ~
+            {{ data.endDate }}
+          </div>
+          <div class="text-xs mb-2">
+            <span class="font-bold">신청기간:</span> {{ data.deadlineDate }}
+            <span class="text-red-500 font-bold">({{data.applicationPeriod}}일 후 마감)</span>
+          </div>
+          <div class="text-xs">
+            <span class="font-bold">모집인원:</span> {{ data.applicantCount }}명
+            / {{ data.applicantMaxCount }}명
+          </div>
+        </div>
+        <div class="flex flex-row px-3 mt-2 pb-2">
+          <span
+            v-for="(item, index) in data.keywords"
+            :key="index"
+            class="
+              inline-block
+              bg-gray-200
+              rounded-full
+              px-2
+              py-1
+              text-xs
+              font-semibold
+              text-gray-700
+              mr-2
+              mb-2
+            "
+          >
+            #{{ item }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
+<style scoped>
+  #title {
+   overflow: hidden;
+   text-overflow: ellipsis;
+   display: -webkit-box;
+   -webkit-line-clamp: 1; /* number of lines to show */
+   -webkit-box-orient: vertical;
+  }
+
+  #description {
+   overflow: hidden;
+   text-overflow: ellipsis;
+   display: -webkit-box;
+   -webkit-line-clamp: 2; /* number of lines to show */
+   -webkit-box-orient: vertical;
+  }
+</style>
+
 <script lang="ts">
+import { reactive, ref, onMounted, computed } from "vue";
+import moment from "moment";
+
 export default {
   props: {
     item: {
       type: Object as any,
       required: true,
     },
+  },
+  setup({ item }) {
+    const data = computed(() => {
+      return {
+        ...item,
+        startDate: moment(item.startDate.toDate()).format("YYYY-MM-DD"),
+        endDate: moment(item.endDate.toDate()).format("YYYY-MM-DD"),
+        deadlineDate: moment(item.deadlineDate.toDate()).format("YYYY-MM-DD"),
+        applicationPeriod: moment(item.deadlineDate.toDate()).diff(
+          moment(),
+          "days"
+        ),
+      };
+    });
+
+    return { data };
   },
 };
 </script>
